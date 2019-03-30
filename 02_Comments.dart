cf52d06901114b6b7135c478795446c8595e1f8e
Gopi chand <vgopichand09@gmail.com>
2019-03-30 15:08:11 +0530

    Create 02_Comments.dart

---
+++ 02_Comments.dart
@@ -0,0 +1,17 @@
+
+/*
+ * This is My First Application In Dart
+ * */
+
+void main(){
+		
+    // This is the first line
+    print("Hello");	//This is the Hello World
+    print(4);
+    
+    //	Multiplication 
+    print(4*2);
+    
+    //Bollean Value
+    print(true);
+  }

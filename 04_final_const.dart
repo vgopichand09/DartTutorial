a980545e3cf9271312afe7002695bddf0880c6fd
Gopi chand <vgopichand09@gmail.com>
2019-04-01 10:18:55 +0530

    Create 04_final_const.dart

---
+++ 04_final_const.dart
@@ -0,0 +1,21 @@
+void main(){
+
+
+//final keyword
+final cityName = "Vijayawada";
+
+//cityName = "Guntur";
+
+final String countyName = "India";
+
+//contant keyword
+const PI= 3.14;
+const double gravity = 3.6;
+}
+
+class Circle{
+
+  final color = "red";
+  static const = "green";
+
+}
